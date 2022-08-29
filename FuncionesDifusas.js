14b795b356eaa485d8ef2157289a392ae4dd4517
Carlos Brys <carlos.brys@gmail.com>
2022-08-29 10:10:54 -0300

    Update FuncionesDifusas.js
    Se agregan las funciones y etiquetas para el viento

--- FuncionesDifusas.js
+++ FuncionesDifusas.js
@@ -1,14 +1,55 @@
 
-  ///////////////////////////////////////////////////////
-  // Funciones difusas para las etiquetas linguisticas //
-  ///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+// Funciones difusas para las etiquetas linguisticas //
+///////////////////////////////////////////////////////
 
+// Temperatura
+//------------
+// Etiquetas:
+// Ux=[-90,55]
+//
   function EtiquetaTemperatura(temperatura) {
-    console.log('temp',temperatura);
-
     txt = '';
     if (temperatura >0 && temperatura <20){ txt = 'Frio';}
     if (temperatura >=20 && temperatura <40){ txt = 'Calor';}
-    console.log('txt',txt);
     return(txt);
   }
+
+// Humedad
+//------------
+  function EtiquetaHumedad(humedad) {
+    txt = '';
+        return(txt);
+  }
+
+// Presion
+//------------
+  function EtiquetaPresion(presion) {
+    txt = '';
+        return(txt);
+  }
+
+// Velocidad del Viento
+//---------------------
+// Escala de Beaufort
+// Km/h	    Denominacion
+// 0 a 1	    Calma
+// 2 a 5	    Ventolina
+// 6 a 11	  Brisa muy débil
+// 12 a 19	  Brisa Ligera
+// 20 a 28	  Brisa moderada
+// 29 a 38	  Brisa fresca
+// 39 a 49	  Brisa fuerte
+// 50 a 61	  Viento fuerte
+// 62 a 74	  Viento duro
+// 75 a 88	  Viento muy duro
+// 89 a 102	Temporal
+// 103 a 117	Borrasca
+// + 118	    Huracán
+//
+  function EtiquetaViento(velocidadViento) {
+        txt = '';
+        return(txt);
+  }
+
+
