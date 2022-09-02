79de437377b98b7af25631bfd0b5c0d8f7e0fb38
Carlos Brys <carlos.brys@gmail.com>
2022-09-02 18:17:41 -0300

    Update FuncionesDifusas.js

--- FuncionesDifusas.js
+++ FuncionesDifusas.js
@@ -83,7 +83,7 @@
 // Direccion del Viento: 0 - 360º
 // Etiquetas: Rosa de los vientos
 //------------
-  function EtiquetaDireccionViento(direccionViento) {
+  function EtiquetaDirViento(direccionViento) {
     txt = '';
 if (direccionViento > 348.75 &&  direccionViento <= 11.25) {
         txt =  "N";
